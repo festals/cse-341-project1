75138eaca650c372011e72911c69473588259790
festals <festals90@gmail.com>
2025-11-11 13:23:27 +0100

    contact part 2 : : crud endpoints, api-docs and swagger

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
-const app = express();
 const mongodb = require('./data/database');
+const bodyParser = require('body-parser');
+
+const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+app.use((req,res,next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, 2-key'
+    );
+    res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 app.use('/', require('./routes'));
 
 mongodb.initDb((err) => {
@@ -14,4 +26,3 @@ mongodb.initDb((err) => {
         app.listen(port, () =>{console.log(`Database is listening and node Running on port ${port}`)});
     }
 });
-
